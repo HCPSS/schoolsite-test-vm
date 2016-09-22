3215336ca79f7041475d3563a1be52a85a467a56
Brendan Anderson <brendan_anderson@hcpss.org>
2016-09-22 14:00:15 -0400

    Change data structure

--- database/README.md
+++
@@ -1,6 +0,0 @@
-Put your gzipped schoolsite database backups in this folder. Use this naming convention:
-
-```
-<school-code>.bak.sql.gz
-```
-
--- utilities/process_backup.py
+++ utilities/process_backup.py
@@ -1,80 +1,121 @@
 #!/usr/bin/env python
 import os, shutil, tarfile
 from lib import BackupOptionResolver
+from multiprocessing import Process
+import tempfile
 
 options = BackupOptionResolver()
 
-class DrupalSourceMover(object):    
+class DrupalSourceMover(object):
     def __init__(self, tar_location, school_code):
         """
         :type tar_location: String - The absolute path to the tarball.
-        :type school_code: String - School code, for example bses, fes, ahs.        
+        :type school_code: String - School code, for example bses, fes, ahs.
         """
         self.tar_location   = tar_location
         self.school_code    = school_code
         self.tar_directory  = os.path.dirname(tar_location)
-        
-    def find_drupal_install(self):
+
+    def find_drupal_install(self, temp_dir):
         """Sometimes drupal is in the archive in /var/www/<school-code>/drupal
         and sometimes it's in /var/www/<school-code>/docroot. I just want to
         find which it is and encapsulate that logic, here.
         """
         known_path = os.path.join(
-            self.tar_directory, 
-            "var", 
-            "www", 
+            temp_dir,
+            "var",
+            "www",
             "{0}.hcpss.org".format(self.school_code)
         )
-        
+
         candidates = [
             os.path.join(known_path, "drupal"),
             os.path.join(known_path, "docroot"),
         ]
-        
+
         for candidate in candidates:
             if os.path.isdir(candidate):
                 return candidate
-        
+
     def move(self, destination):
         """Move the drupal source code to a new location
         :type destination: String - The the root folder. Drupal will be placed
         in a directory INSIDE this directory named after the school code. So,
         don't include the school code here.
-        """        
+        """
+        temp_dir = tempfile.mkdtemp()
         tar = tarfile.open(self.tar_location)
-        tar.extractall(self.tar_directory)
+        tar.extractall(temp_dir)
         tar.close()
-        
-        drupal = self.find_drupal_install();
-        
-        # Sometimes the archive is empty and copytree throws an error so we just 
+
+        drupal = self.find_drupal_install(temp_dir);
+
+        # Sometimes the archive is empty and copytree throws an error so we just
         # want to make sure it exists first.
         if drupal:
             # Sometimes there are more than one backup of a school. copytree
             # throws an arror when this happens, so lets just make sure the
             # destination does not exist before we copy it.
-            if not os.path.isdir(os.path.join(destination, self.school_code)):
-                shutil.copytree(drupal, os.path.join(destination, self.school_code))
-                
-            shutil.rmtree(os.path.join(self.tar_directory, "var"))
+            if not os.path.isdir(destination):
+                shutil.copytree(drupal, destination)
 
+        shutil.rmtree(temp_dir)
+
+def move_sql(school_code):
+    """Move the SQL tarball
+    :type school_code: String - The school code.
+    """
+    directory = os.path.join(options.destination(), school_code)
+    filename = "{0}.bak.sql.gz".format(school_code)
+    create_path(directory)
+    shutil.copy(path, os.path.join(directory, filename))
+    print("Moved SQL for {0}".format(school_code))
+
+def move_data(path, school_code):
+    """Move the drupal source code.
+    :type path: String - The path to the drupal source.
+    :type school_code: String - The school code.
+    """
+    drupal = DrupalSourceMover(path, school_code)
+    directory = os.path.join(options.destination(), school_code)
+    create_path(directory)
+    drupal.move(os.path.join(directory, "drupal"))
+    print("Moved data for {0}".format(school_code))
+
+def create_path(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except (OSError):
+            # Sometimes different threads are checking the existance of the same
+            # directory. This can cause errors, but we don't really care.
+            pass
+
+def process_file(filename, path, school_code):
+    """Process a tarball that is either a sql dump or drupal source code.
+    :type filename: String - The path to the tarball.
+    :type path: String - The path to the drupal source.
+    :type school_code: String - The school code.
+    """
+    if (filename.endswith(".sql.gz")):
+        move_sql(school_code)
+    elif (filename.endswith(".tar.gz")):
+        move_data(path, school_code)
+
+processes = []
 for subdir, dirs, files in os.walk(options.backup_location()):
     for filename in files:
-        path        = os.path.join(subdir, filename)
-        school_code = filename.split("-")[0].split("_")[-1];
-        
+        path = os.path.join(subdir, filename)
+        school_code = filename.split("-")[0].split("_", 2)[-1]
+
+
         if school_code == "school":
             # Indicative of a test site
             continue
-        
-        if (filename.endswith(".sql.gz")):
-            # This is a sql dump
-            destination = "{0}/database/{1}.bak.sql.gz".format(
-                options.destination(), 
-                school_code)
-            
-            shutil.copy(path, destination)
-        elif (filename.endswith(".tar.gz")):
-            drupal = DrupalSourceMover(path, school_code)
-            destination = os.path.join(options.destination(), "data")
-            drupal.move(destination)
+
+        p = Process(target=process_file, args=(filename, path, school_code,))
+        p.start()
+        processes.append(p)
+
+for p in processes:
+    p.join()
